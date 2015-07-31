dd259c774aef01144f801854faa92e50d0f89d84
Daniel Noyola <danielnv18@gmail.com>
2015-07-30 18:35:40 -0600

    Source code for Grun Drupal Task - Drupal Camp CR 2015.

---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+require 'compass/import-once/activate'
+
+# Set this to the root of your project when deployed:
+http_path = "sites/all/themes/demo_theme"
+css_dir = "src/themes/demo_theme/css"
+sass_dir = "src/themes/demo_theme/sass"
+images_dir = "src/themes/demo_theme/img"
+javascripts_dir = "src/themes/demo_theme/js"
+fonts_dir = "src/themes/demo_theme/fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
---
+++ src/project.make
@@ -0,0 +1,17 @@
+core = 7.x
+api = 2
+
+; Drupal Core
+projects[drupal][version] = "7.38"
+
+; =====================================
+; Contrib Modules
+; =====================================
+
+; By default, store all contrib modules in the "contrib" subdirectory of the
+; modules directory.
+defaults[projects][subdir] = "contrib"
+
+projects[ctools][version] = "1.4"
+projects[devel][version] = "1.5"
+projects[views][version] = "3.8"
